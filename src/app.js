90e01e85714ad5efc555c75747b178cbecac577a
Prakhar <mprakhar117@gmail.com>
2021-02-22 17:03:23 +0530

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -91,6 +93,6 @@ app.get('*',(req,res)=>{
         errorMessage:'Page not found'
     })
 })
-app.listen(3000,()=>{
-    console.log('server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('server is up on port port '+port)
 })
